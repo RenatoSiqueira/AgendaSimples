b68681c562de20ae506c1f14b66f15dc5ffb1248
renatosiqueira <renatoelysiqueira@gmail.com>
2020-12-11 18:21:25 -0300

    refact

--- pages/api/post-calendar.js
+++ pages/api/post-calendar.js
@@ -2,24 +2,26 @@ const { addMinutes } = require('date-fns')
 const { google } = require('googleapis')
 import { fromBase64 } from "../../lib/base64";
 
+const credentials = {
+  client_email: process.env.CLIENT_EMAIL,
+  private_key: fromBase64(process.env.PRIVATE_KEY),
+}
+
 export default async (req, res) => {
 
   const client = await google.auth.getClient({
-    credentials: {
-      client_email: process.env.CLIENT_EMAIL,
-      private_key: fromBase64(process.env.PRIVATE_KEY),
-    },
+    credentials,
     scopes: ['https://www.googleapis.com/auth/calendar'],
+    subject: process.env.CLIENT_EMAIL
   });
 
-  client.subject = process.env.CLIENT_EMAIL
   const calendarId = process.env.CALENDAR_ID
   const calendar = google.calendar({ version: 'v3', auth: client })
 
   const data = JSON.parse(req.body);
 
-  await calendar.events.insert(
-    {
+  try {
+    const newEvent = {
       calendarId,
       resource: {
         start: {
@@ -35,7 +37,10 @@ export default async (req, res) => {
         description: `Agendado para ${data.selectedPeople} pessoa(s)`,
       },
     }
-  )
 
-  res.json({ status: true })
+    await calendar.events.insert(newEvent)
+    res.json({ status: true })
+  } catch (error) {
+
+  }
 }
