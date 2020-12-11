6a962bd2817e43a5ca47f4f9e64e8f7f29d91d68
renatosiqueira <renatoelysiqueira@gmail.com>
2020-12-11 18:14:50 -0300

    fix

--- pages/api/get-calendar.js
+++ pages/api/get-calendar.js
@@ -2,35 +2,38 @@ const { addWeeks } = require('date-fns')
 const { google } = require('googleapis')
 import { fromBase64 } from "../../lib/base64";
 
+const credentials = {
+  client_email: process.env.CLIENT_EMAIL,
+  private_key: fromBase64(process.env.PRIVATE_KEY),
+}
+
 export default async (req, res) => {
 
   const client = await google.auth.getClient({
-    credentials: {
-      client_email: process.env.CLIENT_EMAIL,
-      private_key: fromBase64(process.env.PRIVATE_KEY),
-    },
+    credentials,
     scopes: ['https://www.googleapis.com/auth/calendar'],
+    subject: process.env.CLIENT_EMAIL
   });
 
-  client.subject = process.env.CLIENT_EMAIL
+  // client.subject = process.env.CLIENT_EMAIL
   const calendarId = process.env.CALENDAR_ID
   const calendar = google.calendar({ version: 'v3', auth: client })
 
-  await calendar.events.list(
-    {
-      calendarId,
-      timeMin: new Date().toISOString(),
-      timeMax: addWeeks(new Date(), 1).toISOString(), // Let's get events for one week
-      singleEvents: true,
-      orderBy: 'startTime',
-    },
-    (error, result) => {
-      if (error) {
-        console.log(`The API returned an error: ${error}`)
-        res.json(error.message)
-      }
-      // console.log(result?.data) // All data
-      const appointments = await result?.data?.items.map((appointment) => ({
+  const options = {
+    calendarId,
+    timeMin: new Date().toISOString(),
+    timeMax: addWeeks(new Date(), 1).toISOString(), // Let's get events for one week
+    singleEvents: true,
+    orderBy: 'startTime',
+  }
+
+  const callback = (error, result) => {
+    if (error) {
+      console.log(`The API returned an error: ${error}`)
+      res.send(error.message)
+    }
+
+    const appointments = result?.data?.items.map((appointment) => ({
         start: appointment.start.dateTime || appointment.start.date,
         end: appointment.end.dateTime || appointment.end.date,
         id: appointment.id,
@@ -38,7 +41,9 @@ export default async (req, res) => {
         creator: appointment.creator,
         description: appointment.description,
       }))
-      res.json(appointments)
-    },
-  )
+
+    res.send(appointments)
+  }
+
+  await calendar.events.list(options, callback)
 }
