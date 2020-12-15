1ce740774f7249d8b2f9f7e940853e296bd8ff89
renatosiqueira <renatoelysiqueira@gmail.com>
2020-12-15 09:57:35 -0300

    fixes

--- pages/api/get-calendar.js
+++ pages/api/get-calendar.js
@@ -1,6 +1,6 @@
-const { addWeeks } = require('date-fns')
-const { google } = require('googleapis')
-import { fromBase64 } from "../../lib/base64";
+const { addWeeks } = require("date-fns")
+const { google } = require("googleapis")
+import { fromBase64 } from "../../lib/base64"
 
 const credentials = {
   client_email: process.env.CLIENT_EMAIL,
@@ -8,22 +8,21 @@ const credentials = {
 }
 
 export default async (req, res) => {
-
   const client = await google.auth.getClient({
     credentials,
-    scopes: ['https://www.googleapis.com/auth/calendar'],
-    subject: process.env.CLIENT_EMAIL
-  });
+    scopes: ["https://www.googleapis.com/auth/calendar"],
+    subject: process.env.CLIENT_EMAIL,
+  })
 
   const calendarId = process.env.CALENDAR_ID
-  const calendar = google.calendar({ version: 'v3', auth: client })
+  const calendar = google.calendar({ version: "v3", auth: client })
 
   const options = {
     calendarId,
     timeMin: new Date().toISOString(),
     timeMax: addWeeks(new Date(), 4).toISOString(), // Let's get events for four weeks
     singleEvents: true,
-    orderBy: 'startTime',
+    orderBy: "startTime",
   }
 
   const callback = (error, result) => {
@@ -32,12 +31,12 @@ export default async (req, res) => {
       res.send(error.message)
     }
 
-    const appointments = result?.data?.items.map((appointment) => ({
-        start: appointment.start.dateTime || appointment.start.date,
-        end: appointment.end.dateTime || appointment.end.date,
+    const appointments = result.data.items.map((appointment) => ({
+      start: appointment.start.dateTime || appointment.start.date,
+      end: appointment.end.dateTime || appointment.end.date,
       id: appointment.id,
-        description: appointment.description,
-      }))
+      description: appointment.description,
+    }))
 
     res.send(appointments)
   }
